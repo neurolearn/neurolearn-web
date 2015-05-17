ca7ec298c4e5021ed9902aa5c92aa241217427f3
burnash <anton.burnashev@gmail.com>
2015-05-17 17:35:11 +0300

    added eslint

--- app/src/scripts/App.js
+++ app/src/scripts/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-      <h1>need mor colors e react tutorials</h1>
+      <h1>Neurolearn</h1>
     );
   }
 }
