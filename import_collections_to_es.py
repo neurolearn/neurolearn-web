576adc5b7fa539fa8dae3ff551b5f377eeff6c84
burnash <anton.burnashev@gmail.com>
2015-07-11 02:03:00 +0300

    added es mapping and es indexing of collections

--- import_collections_to_es.py
+++ import_collections_to_es.py
@@ -8,11 +8,48 @@ import json
 import requests
 import requests_cache
 
+from elasticsearch import Elasticsearch
+
 
 BASE_URL = "http://neurovault.org/api"
 COLLECTIONS_URL = BASE_URL + "/collections"
 COLLECTIONS_IMAGES_URL = os.path.join(COLLECTIONS_URL, '%s', 'images')
 IMAGES_URL = BASE_URL + "/images"
+
+ES_COLLECTION_MAPPING = {
+    "collection": {
+        "properties": {
+            "images": {
+                "type": "nested",
+                "properties": {
+                        "cognitive_paradigm_cogatlas": {"type": "string"},
+                        "cognitive_paradigm_cogatlas_id": {"type": "string"},
+                        "file": {"type": "string"},
+                        "modality": {"type": "string"},
+                        "contrast_definition_cogatlas": {"type": "string"},
+                        "contrast_definition": {"type": "string"},
+                        "figure": {"type": "string"},
+                        "add_date": {"type": "date"},
+                        "smoothness_fwhm": {"type": "float"},
+                        "brain_coverage": {"type": "float"},
+                        "reduced_representation": {"type": "string"},
+                        "perc_bad_voxels": {"type": "float"},
+                        "description": {"type": "string"},
+                        "statistic_parameters": {"type": "float"},
+                        "map_type": {"type": "string"},
+                        "not_mni": {"type": "boolean"},
+                        "name": {"type": "string"},
+                        "url": {"type": "string"},
+                        "thumbnail": {"type": "string"},
+                        "modify_date": {"type": "date"},
+                        "perc_voxels_outside": {"type": "float"},
+                        "is_thresholded": {"type": "boolean"}
+                }
+            }
+        }
+    }
+}
+
 
 TC_RE = re.compile(r'temporary collection')
 
@@ -61,19 +98,52 @@ def append_collection_images(iterator):
         yield add_images(collection)
 
 
-def main():
+def add_to_es_index(iterator):
+    return iterator
+
+
+def output_collection(collections, collection_id):
+    c504 = [x for x in collections if x['id'] == 504][0]
+    print json.dumps(c504, indent=4, separators=(',', ': '))
+
+
+def get_collections(url=COLLECTIONS_URL):
     pipeline = (fetch_collection_batches,
                 join_batches,
                 filter_temporary_collections,
-                append_collection_images,
-                list)
+                append_collection_images)
 
-    collections = reduce(lambda x, y: y(x), pipeline, COLLECTIONS_URL)
+    return reduce(lambda x, y: y(x), pipeline, url)
 
-    print len(collections)
 
-    c504 = [x for x in collections if x['id'] == 504][0]
-    print json.dumps(c504, indent=4, separators=(',', ': '))
+def create_es_index(client, index):
+    client.indices.create(index)
+    client.indices.put_mapping(
+        index=index, doc_type='collection', body=ES_COLLECTION_MAPPING)
+
+
+def main():
+    es = Elasticsearch()
+    index = 'neurolearn'
+
+    create_es_index(es, index)
+
+    collections = get_collections()
+
+    for ok, result in streaming_bulk(
+        es,
+        collections,
+        index=index,
+        doc_type='collection',
+        chunk_size=50  # keep the batch sizes small for appearances only
+    ):
+        action, result = result.popitem()
+        doc_id = result['_id']
+
+        if not ok:
+            print 'Failed to %s document %s: %r' % (action, doc_id, result)
+        else:
+            print doc_id
 
 
 if __name__ == '__main__':
