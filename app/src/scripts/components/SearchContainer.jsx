07f3172accbe9028ec1814d6e8d0b614b2eb0544
burnash <anton.burnashev@gmail.com>
2015-07-18 19:55:12 +0300

    output total hits number; handle empty search

--- app/src/scripts/components/SearchContainer.jsx
+++ app/src/scripts/components/SearchContainer.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import request from 'superagent';
-import { debounce } from 'lodash';
+import debounce from 'lodash/function/debounce';
 
 import React from 'react';
 import SearchInput from './SearchInput';
@@ -23,27 +23,33 @@ export default class SearchContainer extends React.Component {
 
   loadSearchResults() {
     console.log('search');
-    request.post('/search')
-      .send({
-        query: {
+
+    const query = this.state.searchQuery ?
+        {
           'multi_match': {
             query: this.state.searchQuery,
             fields: ['name', 'authors']
           }
-        },
+        } :
+        undefined;
+
+    request.post('/search')
+      .send({
+        query: query,
+
         size: 5,
         sort: { 'modify_date': { 'order': 'desc'}}
       })
       .type('json')
       .accept('json')
       .end(function(err, res) {
-        console.log(res);
+        console.log(res.body);
         this.setState({searchResults: res.body});
       }.bind(this));
   }
 
   componentDidMount() {
-    this.debouncedLoadSearchResults = debounce(this.loadSearchResults, 700);
+    this.debouncedLoadSearchResults = debounce(this.loadSearchResults, 300);
     this.loadSearchResults();
   }
 
@@ -54,6 +60,10 @@ export default class SearchContainer extends React.Component {
     this.debouncedLoadSearchResults();
   }
 
+  totalHits(searchResults) {
+    return searchResults ? searchResults.hits.total : 0;
+  }
+
   render() {
     return (
       <div className={styles.root}>
@@ -61,13 +71,13 @@ export default class SearchContainer extends React.Component {
           <div className="col-md-9">
             <SearchInput value={this.state.searchQuery} onChange={this.handleSearchInputChange.bind(this)} />
             <div className="search-meta clearfix">
-              <div className="pull-left HitsCount">Found 0 collections</div>
+              <div className="pull-left HitsCount">Found {this.totalHits(this.state.searchResults)} collections</div>
               <div className="pull-right">
                 <SortSearchResults/>
               </div>
             </div>
             <SearchResults results={this.state.searchResults} />
-            <SearchPagination />
+            <SearchPagination total/>
           </div>
           <div className="col-md-3">
             <RefineSearchResults />
