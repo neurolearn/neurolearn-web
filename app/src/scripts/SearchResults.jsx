0c98a8bba497f90e8b9016a8285e862911807339
burnash <anton.burnashev@gmail.com>
2015-07-18 16:17:58 +0300

    margin search results

--- app/src/scripts/SearchResults.jsx
+++ app/src/scripts/SearchResults.jsx
@@ -3,16 +3,18 @@
 import React from 'react';
 import SearchResult from './SearchResult';
 
+import styles from './SearchResults.scss';
+
 export default class SearchResults extends React.Component {
 
   render() {
     var { results } = this.props;
     return results ? (
-      <div>
+      <div className={styles.root}>
         {results.hits.hits.map(hit => <SearchResult hit={hit} />)}
       </div>
     ) : (
-      <div>
+      <div className={styles.root}>
         <h3>No collections found</h3>
       </div>
     );
