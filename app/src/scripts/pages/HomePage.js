74be2083241835bf1a4a4c16aaff61bdd533526b
burnash <anton.burnashev@gmail.com>
2015-08-07 21:13:19 +0300

    added log out link

--- app/src/scripts/pages/HomePage.js
+++ app/src/scripts/pages/HomePage.js
@@ -5,7 +5,7 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Homepage</h1>
+        <h1>Dashboard</h1>
         <Link to="/train-model">Train Model</Link>
       </div>
     );
