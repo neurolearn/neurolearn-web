0fe013dbe14cc6f8a12310ebdcb4a825520aea90
burnash <anton.burnashev@gmail.com>
2015-08-19 02:40:42 +0300

    peeled off div

--- app/src/scripts/components/RunAnalysisForm.js
+++ app/src/scripts/components/RunAnalysisForm.js
@@ -31,23 +31,21 @@ export default class RunAnalysisForm extends React.Component {
     });
 
     return (
-      <div className="RunAnalysisForm">
-        <form className="form-inline" onSubmit={this.handleSubmit.bind(this)}>
-          <div className="form-group">
-            <p></p>
-            <select className="form-control"
-                    ref="algorithmInput"
-                    onChange={this.handleInputChange.bind(this)}
-                    style={{marginRight: 10}}>
-              <option value="">Select an Algorithm</option>
-              <option value="svm">svm</option>
-              <option value="svr">svr</option>
-              <option value="ridge">ridge</option>
-            </select>
-            <button type="submit" className={classes}>Run Analysis</button>
-          </div>
-        </form>
-      </div>
+      <form className="form-inline" onSubmit={this.handleSubmit.bind(this)}>
+        <div className="form-group">
+          <p></p>
+          <select className="form-control"
+                  ref="algorithmInput"
+                  onChange={this.handleInputChange.bind(this)}
+                  style={{marginRight: 10}}>
+            <option value="">Select an Algorithm</option>
+            <option value="svm">svm</option>
+            <option value="svr">svr</option>
+            <option value="ridge">ridge</option>
+          </select>
+          <button type="submit" className={classes}>Run Analysis</button>
+        </div>
+      </form>
     );
   }
 }
