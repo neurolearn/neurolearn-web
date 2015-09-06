d9b4f0724350cf4b744c3563cc833f0a22295d40
burnash <anton.burnashev@gmail.com>
2015-09-07 01:59:31 +0300

    update search; added model panel

--- app/src/scripts/pages/TestModel.js
+++ app/src/scripts/pages/TestModel.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 
 import SearchContainer from '../components/SearchContainer';
 import SelectedCollectionList from '../components/SelectedCollectionList';
 
+import {
+  loadSearchResults,
+  inputSearchQuery,
+} from '../state/search';
+
 export default class TestModel extends React.Component {
   static propTypes = {
+    search: PropTypes.object,
+    selectedImages: PropTypes.object,
+    dispatch: PropTypes.func.isRequired
   };
+
+  componentDidMount() {
+    if (!this.props.search.results) {
+      this.props.dispatch(loadSearchResults(inputSearchQuery('')));
+    }
+  }
 
   countSelectedInCollection(collection) {
     if (!collection) {
@@ -24,6 +38,10 @@ export default class TestModel extends React.Component {
     0);
   }
 
+  handleCollectionClick(id) {
+    this.props.dispatch(showSelectImagesModal(id));
+  }
+
   render() {
     const { selectImagesModal, selectedImages, dispatch } = this.props;
     const anySelected = this.countSelectedImages(selectedImages.images) === 0;
@@ -31,15 +49,14 @@ export default class TestModel extends React.Component {
     return (
       <div>
         <h1 className="page-header">Test Model</h1>
+        <p className="lead">Search NeuroVault collections and select images to test a model on.</p>
+
         <div className="row">
           <div className="col-md-9">
             <div className="panel panel-default">
               <div className="panel-body">
                 <SearchContainer {...this.props.search}
-                                 onFilterChange={this.handleFilterChange.bind(this)}
-                                 onSearchInputChange={this.handleSearchInputChange.bind(this)}
-                                 onSortSelect={this.handleSortSelect.bind(this)}
-                                 onPageSelect={this.handlePageSelect.bind(this)}
+                                 dispatch={this.props.dispatch}
                                  onSearchResultClick={this.handleCollectionClick.bind(this)} />
               </div>
             </div>
@@ -48,8 +65,18 @@ export default class TestModel extends React.Component {
           <div className="col-md-3">
             <div className="panel panel-default">
               <div className="panel-heading">
+                <h3 className="panel-title">Model</h3>
+              </div>
+              <div className='panel-body'>
+                <p>Some model</p>
+              </div>
+            </div>
+
+            <div className="panel panel-default">
+              <div className="panel-heading">
                 <h3 className="panel-title">Selected Images</h3>
               </div>
+              {/*
               <div className={classNames('panel-body', anySelected && 'empty-dataset')}>
                 { anySelected
                   ? <p>Training dataset is empty.</p>
@@ -57,6 +84,7 @@ export default class TestModel extends React.Component {
                                onItemClick={(id) => this.handleCollectionClick(id)} />
                 }
               </div>
+              */}
             </div>
           </div>
         </div>
