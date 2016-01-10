138ed89e183b19524e6fb277f10da2697087f58e
burnash <anton.burnashev@gmail.com>
2016-01-11 01:40:20 +0300

    add config for production

--- app/src/scripts/constants/auth.js
+++ app/src/scripts/constants/auth.js
@@ -1,5 +1,5 @@
 export const JWT_KEY_NAME = 'jwt';
 export const NEUROVAULT_CLIENT_IDS = {
   development: 'q5avszwASkC3WNywlGOgQYgiztNStiLbdy80izw8',
-  production: ''
+  production: 'PuSzHaE4sW2pwCetMgvlpNxPWXF5OufE27uKPDhi'
 };
