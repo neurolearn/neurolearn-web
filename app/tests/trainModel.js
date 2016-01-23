65cd7de65686b7b05bcf9489e25f44168a54819b
burnash <anton.burnashev@gmail.com>
2016-01-24 02:35:45 +0300

    setup nightwatch

---
+++ app/tests/trainModel.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'Sign In': function (browser) {
+    browser
+      .url('http://localhost:3000')
+      .waitForElementVisible('h1', 1000)
+      .click('a[href^=\'http://neurovault.org\']')
+      .waitForElementVisible('input[id=\'id_username\']', 1000)
+      .setValue('input[id=\'id_username\']', 'testuser')
+      .setValue('input[id=\'id_password\']', 'testuser')
+      .click('input[type=submit]')
+      .pause(1000)
+      .end();
+  }
+};
