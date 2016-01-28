b6732bfb9f3c18d6bbf76304cb994717ad50e8f5
burnash <anton.burnashev@gmail.com>
2016-01-28 21:19:01 +0300

    factor out routes

--- app/src/scripts/index.js
+++ app/src/scripts/index.js
@@ -7,50 +7,14 @@ require('handsontable.full.css');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect } from 'react-router';
 import { history } from 'react-router/lib/HashHistory';
 import { Provider } from 'react-redux';
 
-import App from './App';
-import HomePage from './pages/HomePage';
-import Explore from './pages/Explore';
-import MLModels from './pages/dashboard/MLModels';
-import ModelTests from './pages/dashboard/ModelTests';
-import TrainModel from './pages/TrainModel';
-import ViewModel from './pages/ViewModel';
-import TestModel from './pages/TestModel';
-import ViewTest from './pages/ViewTest';
-import InputData from './components/InputData';
-import TrainingLabel from './components/TrainingLabel';
-import ModelPreferences from './components/ModelPreferences';
-import NotFound from './pages/NotFound';
+import renderRoutes from './routes';
+
 import configureStore from './store/configureStore';
 
 const store = configureStore();
-
-function renderRoutes(history) {
-  return (
-    <Router history={history}>
-      <Route component={App}>
-        <Route path="/" component={HomePage}/>
-        <Route path="/dashboard/models" component={MLModels} />
-        <Route path="/dashboard/tests" component={ModelTests} />
-        <Redirect from="/explore" to="/explore/models" />
-        <Route path="/explore(/:itemType)" component={Explore} />
-        <Redirect from="/models/new" to="/models/new/input-data" />
-        <Route path="/models/new" component={TrainModel}>
-          <Route path="input-data" component={InputData}/>
-          <Route path="training-label" component={TrainingLabel}/>
-          <Route path="model-preferences" component={ModelPreferences}/>
-        </Route>
-        <Route path="/models/:id" component={ViewModel} />
-        <Route path="/tests/new" component={TestModel} />
-        <Route path="/tests/:id" component={ViewTest} />
-        <Route path="*" component={NotFound} />
-      </Route>
-    </Router>
-  );
-}
 
 ReactDOM.render((
   <Provider store={store}>
---
+++ app/src/scripts/routes.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Router, Route, Redirect } from 'react-router';
+
+import App from './App';
+import HomePage from './pages/HomePage';
+import Explore from './pages/Explore';
+import MLModels from './pages/dashboard/MLModels';
+import ModelTests from './pages/dashboard/ModelTests';
+import TrainModel from './pages/TrainModel';
+import ViewModel from './pages/ViewModel';
+import TestModel from './pages/TestModel';
+import ViewTest from './pages/ViewTest';
+import InputData from './components/InputData';
+import TrainingLabel from './components/TrainingLabel';
+import ModelPreferences from './components/ModelPreferences';
+import NotFound from './pages/NotFound';
+
+export default function renderRoutes(history) {
+  return (
+    <Router history={history}>
+      <Route component={App}>
+        <Route path="/" component={HomePage}/>
+        <Route path="/dashboard/models" component={MLModels} />
+        <Route path="/dashboard/tests" component={ModelTests} />
+        <Redirect from="/explore" to="/explore/models" />
+        <Route path="/explore(/:itemType)" component={Explore} />
+        <Redirect from="/models/new" to="/models/new/input-data" />
+        <Route path="/models/new" component={TrainModel}>
+          <Route path="input-data" component={InputData}/>
+          <Route path="training-label" component={TrainingLabel}/>
+          <Route path="model-preferences" component={ModelPreferences}/>
+        </Route>
+        <Route path="/models/:id" component={ViewModel} />
+        <Route path="/tests/new" component={TestModel} />
+        <Route path="/tests/:id" component={ViewTest} />
+        <Route path="*" component={NotFound} />
+      </Route>
+    </Router>
+  );
+}
