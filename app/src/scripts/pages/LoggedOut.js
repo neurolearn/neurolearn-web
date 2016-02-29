16f1ec0fae61f5bcc93a1985911064562b67968f
burnash <anton.burnashev@gmail.com>
2016-02-29 21:23:37 +0300

    fixed case

--- app/src/scripts/pages/LoggedOut.js
+++ app/src/scripts/pages/LoggedOut.js
@@ -9,7 +9,7 @@ export default class LoggedOut extends React.Component {
           <h1 className="hero-header">Neurolearn</h1>
           <p className="lead">A web platform for analyzing neuroimaging data stored in <a href="http://neurovault.org">NeuroVault</a> using machine-learning tools.</p>
         </div>
-        <p>The basic concept is to provide an easy to use interface to allow researchers to develop predictive brain models of psychological states by applying multivariate decoding methods to open source data publicly shared via NeuroVault.  These predictive brain models can be shared with other researchers and also tested on any data available on neurovault.  This can be useful for assessing the sensitivity and specificity of the brain pattern to other psychological states.</p>
+        <p>The basic concept is to provide an easy to use interface to allow researchers to develop predictive brain models of psychological states by applying multivariate decoding methods to open source data publicly shared via NeuroVault.  These predictive brain models can be shared with other researchers and also tested on any data available on NeuroVault.  This can be useful for assessing the sensitivity and specificity of the brain pattern to other psychological states.</p>
         <p>Ultimately, we hope that this tool will contribute to accelerating the development and validation cycle of brain-based markers of psychological states and will aid in promoting a more open and transparent study of the human mind.</p>
         <p>There are currently <Link to="/explore/models">10 models</Link> and <Link to="/explore/tests">10 tests</Link> being shared.</p>
 
