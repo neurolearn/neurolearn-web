e4f8a89e3dad210eed194295b88a1dfaadc19b82
burnash <anton.burnashev@gmail.com>
2016-03-02 22:34:55 +0300

    pure component

--- app/src/scripts/pages/NotFound.js
+++ app/src/scripts/pages/NotFound.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-export default class NotFound extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Page Not Found</h1>
-        <Link to="/">Home</Link>
-      </div>
-    );
-  }
-}
+const NotFound = () => (
+  <div>
+    <h1>Page Not Found</h1>
+    <Link to="/">Home</Link>
+  </div>
+);
+
+export default NotFound;
