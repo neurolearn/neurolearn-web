ebf184a769fc41612ad25f90ceca882f09708cb6
burnash <anton.burnashev@gmail.com>
2016-03-07 03:33:42 +0300

    better tabular display

--- app/src/scripts/components/ModelOverview.js
+++ app/src/scripts/components/ModelOverview.js
@@ -3,24 +3,24 @@ import React, { PropTypes } from 'react';
 import { algorithmNameMap } from '../constants/Algorithms';
 import { pluralize } from '../utils.js';
 
+import styles from './ModelOverview.scss';
+
 const ModelOverview = ({model}) => (
-  <div>
-    <table className="table overview">
-      <thead>
-        <tr>
-          <td className="col-md-4">Algorithm</td>
-          <td className="col-md-4">Training Label</td>
-          <td className="col-md-4">Training Dataset</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{algorithmNameMap[model.algorithm]}</td>
-          <td>{model.label_name}</td>
-          <td>{model.images_count} {pluralize(model.images_count, 'image', 'images')}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div className={styles.root}>
+    <div className="row">
+        <div className="col-md-4 col-xs-12">
+            <div className="attribute-label">Algorithm</div>
+            <p>{algorithmNameMap[model.algorithm]}</p>
+        </div>
+        <div className="col-md-4 col-xs-12">
+            <div className="attribute-label">Training Label</div>
+            <p>{model.label_name}</p>
+        </div>
+        <div className="col-md-4 col-xs-12">
+            <div className="attribute-label">Training Dataset</div>
+            <p>{model.images_count} {pluralize(model.images_count, 'image', 'images')}</p>
+        </div>
+    </div>
   </div>
 );
 
