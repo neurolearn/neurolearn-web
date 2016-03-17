255d6d48c56bfefcb4bcf13479316f4594c00b7b
burnash <anton.burnashev@gmail.com>
2016-03-18 01:46:11 +0300

    add production oauth key

--- app/src/scripts/constants/auth.js
+++ app/src/scripts/constants/auth.js
@@ -1,5 +1,6 @@
 export const JWT_KEY_NAME = 'jwt';
 export const NEUROVAULT_CLIENT_IDS = {
   development: 'q5avszwASkC3WNywlGOgQYgiztNStiLbdy80izw8',
-  production: 'PuSzHaE4sW2pwCetMgvlpNxPWXF5OufE27uKPDhi'
+  staging: 'PuSzHaE4sW2pwCetMgvlpNxPWXF5OufE27uKPDhi',
+  production: 'BtxrWZTlKCgcqOfjr5VqfHJbDReI87hDWqAqa3cI'
 };
