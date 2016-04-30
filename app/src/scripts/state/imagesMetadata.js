67b81c5ff96a8a12ea3f36ea883aab4ee0ff3238
burnash <anton.burnashev@gmail.com>
2016-04-30 12:48:12 +0300

    exclude file_size

--- app/src/scripts/state/imagesMetadata.js
+++ app/src/scripts/state/imagesMetadata.js
@@ -16,7 +16,7 @@ export const RECEIVE_IMAGES_METADATA = 'RECEIVE_IMAGES_METADATA';
 
 const excludeProps = ['collection', 'cognitive_paradigm_cogatlas_id', 'description', 'add_date', 'is_thresholded',
                       'perc_bad_voxels', 'brain_coverage', 'perc_voxels_outside', 'reduced_representation', 'thumbnail',
-                      'not_mni', 'statistic_parameters', 'smoothness_fwhm', 'contrast_definition',
+                      'not_mni', 'statistic_parameters', 'smoothness_fwhm', 'contrast_definition', 'file_size',
                       'contrast_definition_cogatlas', 'figure', 'modify_date'];
 
 const mandatoryProps = ['id', 'collection_id', 'file', 'name'];
