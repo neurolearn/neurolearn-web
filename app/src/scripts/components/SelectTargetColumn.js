5dff99c2b3229c73706bd80801edee517000aaf2
burnash <anton.burnashev@gmail.com>
2016-05-02 01:50:00 +0300

    add edit and append columns features

--- app/src/scripts/components/SelectTargetColumn.js
+++ app/src/scripts/components/SelectTargetColumn.js
@@ -1,9 +1,13 @@
 import includes from 'lodash/collection/includes';
 import take from 'lodash/array/take';
 import React, { PropTypes } from 'react';
-import { getArrayColumns, guessType, findColumnIndex } from '../utils';
+import { getColumnsFromArray, guessType, findColumnIndex } from '../utils';
+import EditColumnModal from './EditColumnModal';
 
-const excludeColumns = ['id', 'collection_id', 'file', 'url', 'file_size'];
+import styles from './SelectTargetColumn.scss';
+
+const excludeColumns = ['id', 'collection_id', 'name', 'file',
+                        'url', 'file_size'];
 
 function getTargetData(data, columnName) {
   const idIndex = findColumnIndex(data, 'id');
@@ -14,7 +18,7 @@ function getTargetData(data, columnName) {
   const trainingData = data
     .slice(1)
     .filter(row => row[idIndex] && row[collectionIdIndex])
-    .map((row, i) => {
+    .map(row => {
       return {
         'id': row[idIndex],
         'subject_id': undefined,
@@ -33,16 +37,30 @@ function getTargetData(data, columnName) {
   };
 }
 
+function pickColumns(data, columnNames) {
+  const indexes = columnNames.map(name => findColumnIndex(data, name));
+
+  return data.map(row => indexes.map(index => row[index]));
+}
+
 export default class SelectTargetColumn extends React.Component {
   static propTypes = {
     data: PropTypes.array,
     targetData: PropTypes.object,
-    onSelectTarget: PropTypes.func.isRequired
+    onSelectTarget: PropTypes.func.isRequired,
+    onColumnSave: PropTypes.func.isRequired
   }
 
   constructor(props) {
     super(props);
+    this.state = {
+      showEditColumnModal: false,
+      editColumnName: null,
+      editColumnData: undefined
+    }
     this.handleRadioChange = this.handleRadioChange.bind(this);
+    this.handleNewColumnAdd = this.handleNewColumnAdd.bind(this);
+    this.handleEditColumnModalHide = this.handleEditColumnModalHide.bind(this);
   }
 
   handleRadioChange(e) {
@@ -51,9 +69,39 @@ export default class SelectTargetColumn extends React.Component {
     onSelectTarget(getTargetData(data, columnName));
   }
 
+  handleNewColumnAdd(e) {
+    e.preventDefault();
+
+    const newColumnData = pickColumns(
+        this.props.data,
+        ['id', 'collection_id', 'name']
+    ).map((row, i) => [...row, i === 0 ? 'values' : '']);
+
+    this.setState({
+      showEditColumnModal: true,
+      editColumnName: '',
+      editColumnData: newColumnData
+    });
+  }
+
+  handleColumnEdit(e, columnName) {
+    this.setState({
+      showEditColumnModal: true,
+      editColumnName: columnName,
+      editColumnData: pickColumns(
+        this.props.data,
+        ['id', 'collection_id', 'name', columnName]
+      )
+    });
+  }
+
+  handleEditColumnModalHide() {
+    this.setState({ showEditColumnModal: false });
+  }
+
   render() {
     const { data, targetData: { trainingLabel } } = this.props;
-    const columns = getArrayColumns(
+    const columns = getColumnsFromArray(
       data
     ).filter(
       column => !includes(excludeColumns, column.name)
@@ -68,6 +116,7 @@ export default class SelectTargetColumn extends React.Component {
     );
 
     return (
+      <div className={styles.root}>
       <table className="table table-hover">
         <thead>
           <tr>
@@ -75,6 +124,7 @@ export default class SelectTargetColumn extends React.Component {
             <th>Name</th>
             <th>Data Type</th>
             <th>Sample Field Values</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -84,7 +134,7 @@ export default class SelectTargetColumn extends React.Component {
                 <td>
                   <input type="radio"
                          value={column.name}
-                         onChange={this.handleRadioChange.bind(this)}
+                         onChange={this.handleRadioChange}
                          checked={column.name === trainingLabel.name}/>
                 </td>
                 <td>
@@ -96,11 +146,30 @@ export default class SelectTargetColumn extends React.Component {
                 <td>
                   {column.sampleValues.join(', ')}
                 </td>
+                <td style={{textAlign: 'right'}}>
+                 <span className="action" onClick={(e) => this.handleColumnEdit(e, column.name)}><i className="fa fa-pencil"></i> Edit</span>
+                 <span className="action"><i className="fa fa-trash"></i> Delete</span>
+                </td>
               </tr>
             )
           }
+          <tr>
+            <td></td>
+            <td colSpan="4">
+              <a href="#" onClick={this.handleNewColumnAdd}>Add new column</a>
+            </td>
+          </tr>
         </tbody>
       </table>
+      {this.state.editColumnData &&
+        <EditColumnModal
+          data={this.state.editColumnData}
+          name={this.state.editColumnName}
+          show={this.state.showEditColumnModal}
+          onHide={this.handleEditColumnModalHide}
+          onSave={this.props.onColumnSave}
+          />}
+      </div>
     );
   }
 }
