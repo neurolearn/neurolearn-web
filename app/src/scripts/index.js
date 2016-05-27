a33af5c586c1089ff1e941673953315c922a5b5b
burnash <anton.burnashev@gmail.com>
2016-05-27 20:23:20 +0300

    use redux persist

--- app/src/scripts/index.js
+++ app/src/scripts/index.js
@@ -9,12 +9,14 @@ import React from 'react';
 import render from 'react-dom';
 import { browserHistory } from 'react-router'
 import { Provider } from 'react-redux';
+import {persistStore, autoRehydrate} from 'redux-persist';
 
 import renderRoutes from './routes';
 
 import configureStore from './store/configureStore';
 
 const store = configureStore();
+persistStore(store);
 
 render.render((
   <Provider store={store}>
--- app/src/scripts/store/configureStore.js
+++ app/src/scripts/store/configureStore.js
@@ -1,14 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
+import {autoRehydrate} from 'redux-persist';
+
 const logger = createLogger({});
 
-const createStoreWithMiddleware = applyMiddleware(
+const _createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware,
   logger
 )(createStore);
+
+const createStoreWithMiddleware = (autoRehydrate())(_createStoreWithMiddleware);
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleware(rootReducer, initialState);
