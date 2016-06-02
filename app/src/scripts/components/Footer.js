e0fdf8feec576384e5b2b0bf96c94db046fc2b38
burnash <anton.burnashev@gmail.com>
2016-06-03 00:58:30 +0300

    add a supporter

--- app/src/scripts/components/Footer.js
+++ app/src/scripts/components/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer text-center">
     <hr/>
-    <p>Created and maintained by Luke Chang, Tor Wager, and Anton Burnashev. Supported by NIH award R01DA035484-02S1</p>
+    <p>Created and maintained by Luke Chang, Tor Wager, and Anton Burnashev.</p>
+    <p>Supported by NIH award R01DA035484-02S1 and a <a href="http://neukom.dartmouth.edu/">Neukom CompX award</a>.</p>
   </footer>
 );
 
