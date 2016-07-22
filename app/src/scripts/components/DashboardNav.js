4bbdefa3c4b920fbb1c8655bc78fcba7ac82252c
burnash <anton.burnashev@gmail.com>
2016-07-22 18:42:02 +0300

    make parameter optional

--- app/src/scripts/components/DashboardNav.js
+++ app/src/scripts/components/DashboardNav.js
@@ -3,7 +3,7 @@
 import React, { PropTypes } from 'react';
 import NavItem from '../components/NavItem';
 
-const DashboardNav = ({children} : {children : Object}) => (
+const DashboardNav = ({children} : {children? : any}) => (
   <ul className="nav nav-tabs">
     <NavItem to="/dashboard/models">Models</NavItem>
     <NavItem to="/dashboard/tests">Tests</NavItem>
