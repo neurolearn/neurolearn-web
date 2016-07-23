dbb186dccd65bbb78b153e9fbde3901dfe935ce9
burnash <anton.burnashev@gmail.com>
2016-07-23 17:20:22 +0300

    add annotations

--- app/src/scripts/components/ImageItem.js
+++ app/src/scripts/components/ImageItem.js
@@ -1,29 +1,35 @@
+/* @flow */
+
 import React, { PropTypes } from 'react';
 import { Input } from 'react-bootstrap';
 import FallbackImage from './FallbackImage';
 
-export default class ImageItem extends React.Component {
-  static propTypes = {
-    checked: PropTypes.bool.isRequired,
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
-  }
+const ImageItem = (
+  { name, checked, onChange, thumbnail, image_type, map_type }
+: { name: string,
+    checked: boolean,
+    onChange: () => void,
+    thumbnail: string,
+    image_type: string,
+    map_type: string }
+) => (
+  <tr>
+    <td>
+      <input type='checkbox' checked={checked} onChange={onChange}/>
+    </td>
+    <td onClick={onChange} className="name">{name}</td>
+    <td>{image_type}</td>
+    <td>{map_type}</td>
+    <td onClick={onChange} className="map-thumbnail">
+      <FallbackImage src={thumbnail} className="img-responsive" />
+    </td>
+  </tr>
+);
 
-  render() {
-    const { name, checked, onChange, thumbnail,
-            image_type, map_type } = this.props;
-    return (
-      <tr>
-        <td>
-          <input type='checkbox' checked={checked} onChange={onChange}/>
-        </td>
-        <td onClick={onChange} className="name">{name}</td>
-        <td>{image_type}</td>
-        <td>{map_type}</td>
-        <td onClick={onChange} className="map-thumbnail">
-          <FallbackImage src={thumbnail} className="img-responsive" />
-        </td>
-      </tr>
-    );
-  }
+ImageItem.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
 }
+
+export default ImageItem;
