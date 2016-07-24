44b23727caac3c9aa0112ff634b58144e559bb97
burnash <anton.burnashev@gmail.com>
2016-07-25 00:36:38 +0300

    add annotations

--- app/src/scripts/components/search/SearchResults.js
+++ app/src/scripts/components/search/SearchResults.js
@@ -1,27 +1,31 @@
+/* @flow */
+
 import React, { PropTypes } from 'react';
 import SearchResult from './SearchResult';
 
 import styles from './SearchResults.scss';
 
-export default class SearchResults extends React.Component {
-  static propTypes = {
-    results: PropTypes.object,
-    onSearchResultClick: PropTypes.func.isRequired
-  }
-
-  render() {
-    var { results } = this.props;
-    return results.hits.total ? (
-      <div className={styles.root}>
-        {results.hits.hits.map(hit =>
-          <SearchResult key={hit._id}
-                        hit={hit}
-                        onClick={() => this.props.onSearchResultClick(hit._id)} />)}
-      </div>
-    ) : (
-      <div className={styles.root}>
-        <h3>No collections found</h3>
-      </div>
-    );
-  }
+const SearchResults = (
+  { results, onSearchResultClick }
+: { results: Object, onSearchResultClick: (id: number) => void }
+) => {
+  return results.hits.total ? (
+    <div className={styles.root}>
+      {results.hits.hits.map(hit =>
+        <SearchResult key={hit._id}
+                      hit={hit}
+                      onClick={() => onSearchResultClick(hit._id)} />)}
+    </div>
+  ) : (
+    <div className={styles.root}>
+      <h3>No collections found</h3>
+    </div>
+  );
 }
+
+SearchResults.propTypes = {
+  results: PropTypes.object,
+  onSearchResultClick: PropTypes.func.isRequired
+};
+
+export default SearchResults;
