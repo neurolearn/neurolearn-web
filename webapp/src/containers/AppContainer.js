98faa41da6c3f5d2f5bfd77b8443379c150eaf4d
burnash <anton.burnashev@gmail.com>
2016-07-27 16:49:31 +0300

    add entry scripts

---
+++ webapp/src/containers/AppContainer.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react'
+// import { Router } from 'react-router'
+import { Provider } from 'react-redux'
+
+class AppContainer extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    routes: PropTypes.object.isRequired,
+    store: PropTypes.object.isRequired
+  }
+
+  render () {
+    const { history, routes, store } = this.props
+
+    return (
+      <Provider store={store}>
+        {/* react-kit <Router history={history} children={routes} /> */}
+        {() => renderRoutes(store, browserHistory)}
+
+      </Provider>
+    )
+  }
+}
+
+export default AppContainer
