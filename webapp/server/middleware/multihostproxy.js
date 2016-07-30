feb4c91f464b296c6735d87991863b9e202f39d9
burnash <anton.burnashev@gmail.com>
2016-07-30 13:25:20 +0300

    refactor

--- webapp/server/middleware/multihostproxy.js
+++ webapp/server/middleware/multihostproxy.js
@@ -5,55 +5,52 @@ import _debug from 'debug';
 
 const debug = _debug('app:server:multiproxy');
 
-const apiHost = 'localhost:3001';
+const API_HOST = 'localhost:3001';
+const NEUROVAULT_HOST = 'neurovault.org';
 
-function apiProxy(req) {
-  var url = req.url;
+const PROXY_URL_MAP = {
+  '/nvproxy': nvApiProxy,
+  '/api': appServerProxy,
+  '/signin': appServerProxy,
+  '/media': appServerProxy,
+  '/static': appServerProxy,
+  '/search': elasticSearchProxy
+};
 
-  var proxied = request('http://' + apiHost + url);
+function appServerProxy(req) {
+  const url = req.url;
+
+  const proxied = request('http://' + API_HOST + url);
   proxied.on('error', function (err) {
-    console.log('Unable to proxy: ', url, err);
+    debug('Unable to proxy: ', url, err);
   });
-
   return proxied;
 }
 
 function nvApiProxy(req) {
-  var nvproxy = req.url.match('^/nvproxy(/.+)');
-  var url = nvproxy[1];
-  var host = 'neurovault.org';
+  const nvproxy = req.url.match('^/nvproxy(/.+)');
+  const url = nvproxy[1];
 
-  var proxied = request('http://' + host + url);
+  const proxied = request('http://' + NEUROVAULT_HOST + url);
   proxied.on('error', function (err) {
-    console.log('Unable to proxy: ', url, err);
+    debug('Unable to proxy: ', url, err);
   });
   return proxied;
 }
 
 function elasticSearchProxy(req) {
-  var proxied = request('http://localhost:9200/neurolearn/collection/_search');
+  const proxied = request('http://localhost:9200/neurolearn/collection/_search');
   proxied.on('error', function (err) {
-    console.log('Unable to proxy: ', req.url, err);
+    debug('Unable to proxy: ', req.url, err);
   });
   return proxied;
 }
 
-var proxyUrlMapping = {
-  '/nvproxy': nvApiProxy,
-  '/api': apiProxy,
-  '/signin': apiProxy,
-  '/media': apiProxy,
-  '/static': apiProxy,
-  '/search': elasticSearchProxy
-};
-
 function dispatchProxy(reqUrl) {
-  var url;
-
-  for (url in proxyUrlMapping) {
-    if (proxyUrlMapping.hasOwnProperty(url)) {
+  for (const url in PROXY_URL_MAP) {
+    if (PROXY_URL_MAP.hasOwnProperty(url)) {
       if (reqUrl.indexOf(url) === 0) {
-        return proxyUrlMapping[url];
+        return PROXY_URL_MAP[url];
       }
     }
   }
@@ -64,10 +61,10 @@ export default function () {
   debug('Enable multi host proxy middleware.');
 
   const middleware = (req, res, next) => {
-    var proxied = dispatchProxy(req.url);
+    const proxied = dispatchProxy(req.url);
 
     if (proxied) {
-      var proxiedRequest = proxied(req);
+      const proxiedRequest = proxied(req);
       req.pipe(proxiedRequest).pipe(res);
     } else {
       return next();
