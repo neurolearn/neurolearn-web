f67af0f465f14e5d1a34e7b45cfaaad0de408c90
burnash <anton.burnashev@gmail.com>
2016-07-31 16:49:27 +0300

    eslint

--- webapp/src/utils/events.js
+++ webapp/src/utils/events.js
@@ -4,13 +4,13 @@
  * Dynamic type test for Event
  * Courtesy of rosskevin (https://github.com/facebook/flow/issues/368#issuecomment-224990683)
  */
-function target<E> (e:SyntheticEvent, t:E) {
+function target<E>(e: SyntheticEvent, t:E) {
   if (e.target instanceof t) {
     // Now we know it's an <input />, with a `value` property.
-    return e.target
+    return e.target;
   } else {
-    throw new Error(`Expected Event target to be ${t} but found ${typeof e.target}`)
+    throw new Error(`Expected Event target to be ${t} but found ${typeof e.target}`);
   }
 }
 
-export default { target }
+export default { target };
