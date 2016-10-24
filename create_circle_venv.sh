3e36053a7f8f7da0dd54f16203ee2b8396a4ccf0
burnash <anton.burnashev@gmail.com>
2016-10-24 22:28:44 +0300

    do not install anything along with conda

--- create_circle_venv.sh
+++ create_circle_venv.sh
@@ -25,7 +25,7 @@ popd
 
 # Configure the conda environment and put it in the path using the
 # provided versions
-conda create -n testenv --yes --quiet python coverage matplotlib pillow
+conda create -n testenv --yes --quiet python
 source activate testenv
 
 pip install -r requirements/dev.txt
