51b5a74a1fec93bea8b707341ebf6caeca11bb4a
burnash <anton.burnashev@gmail.com>
2017-04-26 01:26:44 +0200

    display a lock next to private collections

--- client/src/components/search/SearchResult.js
+++ client/src/components/search/SearchResult.js
@@ -7,34 +7,57 @@ import { pluralize, neuroVaultCollectionURL } from '../../utils';
 
 import styles from './SearchResult.scss';
 
-function firstImageThumbnail(images: Array<{thumbnail: string}>): string {
+function firstImageThumbnail(images: Array<{ thumbnail: string }>): string {
   return images.length ? images[0].thumbnail : '';
 }
 
-const SearchResult = (props : { source: Object, onClick: () => void }) => {
-  const { id, name, number_of_images: numberOfImages, sample_images: images, authors } = props.source;
+const SearchResult = (props: { source: Object, onClick: () => void }) => {
+  const {
+    id,
+    name,
+    number_of_images: numberOfImages,
+    sample_images: images,
+    authors,
+    private: isPrivate
+  } = props.source;
 
   return (
     <div className={styles.root}>
       <div className="row">
         <div className="title">
-          <h3>{name}</h3>
+          <h3>
+            {name}
+            {isPrivate &&
+              <i
+                title="Private collection"
+                className="fa fa-lock"
+                style={{ marginLeft: 4, color: 'gray' }}
+                aria-hidden="true"
+                key="lock"
+              />}
+          </h3>
           <p>{authors}</p>
           <p>
-            <a
-              href={neuroVaultCollectionURL(id)}
-              target="_blank"
-            ><i style={{paddingRight: 2}} className="fa fa-external-link"></i>NeuroVault</a>
+            <a href={neuroVaultCollectionURL(id)} target="_blank">
+              <i style={{ paddingRight: 2 }} className="fa fa-external-link" />
+              NeuroVault
+            </a>
           </p>
         </div>
         <div className="images">
           {images &&
-            <FallbackImage src={firstImageThumbnail(images)} className="img-responsive" />
-          }
-          <div className="number-of-images">{numberOfImages} {pluralize(numberOfImages, 'image', 'images')}</div>
+            <FallbackImage
+              src={firstImageThumbnail(images)}
+              className="img-responsive"
+            />}
+          <div className="number-of-images">
+            {numberOfImages} {pluralize(numberOfImages, 'image', 'images')}
+          </div>
         </div>
         <div className="button">
-          <Button bsStyle="default" onClick={props.onClick}>Select Images</Button>
+          <Button bsStyle="default" onClick={props.onClick}>
+            Select Images
+          </Button>
         </div>
       </div>
     </div>
